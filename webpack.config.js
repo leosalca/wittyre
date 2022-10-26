e9d7face62266b120d3b84fca87d1f12f133b042
Leo S <leo.sal.ca@outlook.com>
2022-10-26 11:47:35 -0600

    Configuring more package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     output: {
         path: path.join(_dirname, "dist"),
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                },
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(_dirname, "src", "index.html"),
